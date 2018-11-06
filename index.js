7bd9271d62a04f829eed12475ddf682853b5a468
Tony Van den Eynde <tony.van.den.eynde@vanhool.be>
2018-11-06 09:03:12 +0100

    use cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-//const cors = require('cors');
+const cors = require('cors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
